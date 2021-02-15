b7e4cc5154a9e34bf35eeb63676a8baf49b3ab70
kutta97 <yhjin04170@gmail.com>
2021-02-16 02:40:09 +0900

    write Include guard and include stdlib & unistd header files. also add get_next_line fuction prototype

--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,16 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 02:28:18 by hyyang            #+#    #+#             */
-/*   Updated: 2021/02/16 02:28:20 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/02/16 02:38:54 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+# include <stdlib.h>
+# include <unistd.h>
+
+int	get_next_line(int fd, char **line);
+
+#endif
