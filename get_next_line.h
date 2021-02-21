5ab7a6180b503d27cf183889d0fd1e46ba0df0d7
kutta97 <yhjin04170@gmail.com>
2021-02-22 02:06:37 +0900

    write FD_SIZE macro and add strjoin(), strchr() prototype

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 02:28:18 by hyyang            #+#    #+#             */
-/*   Updated: 2021/02/16 02:38:54 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/02/21 23:02:09 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,16 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int	get_next_line(int fd, char **line);
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 32
+#endif
+
+#ifndef FD_SIZE
+# define FD_SIZE 256
+#endif
+
+int		get_next_line(int fd, char **line);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c)
 
 #endif
