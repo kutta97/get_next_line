da1d4eb076341cbc62f5fc5d030fc6ca296881d1
kutta97 <yhjin04170@gmail.com>
2021-03-04 01:58:37 +0900

    refactor util functions

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 02:28:35 by hyyang            #+#    #+#             */
-/*   Updated: 2021/03/04 01:29:19 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/03/04 01:58:01 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,24 +22,17 @@ static size_t	ft_strlen(const char *s)
 	return (len);
 }
 
-static size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t			ft_strcpy(char *dst, const char *src)
 {
 	size_t	i;
-	size_t	dst_len;
-	size_t	src_len;
 
 	i = 0;
-	dst_len = ft_strlen(dst);
-	src_len = ft_strlen(src);
-	if (dstsize <= dst_len)
-		return (dstsize + src_len);
-	while (src[i] && dst_len + i < dstsize - 1)
+	while (src[i])
 	{
-		dst[dst_len + i] = src[i];
+		dst[i] = src[i];
 		i++;
 	}
-	dst[dst_len + i] = '\0';
-	return (dst_len + src_len);
+	return (i);
 }
 
 static char		*ft_strdup(const char *s1)
@@ -49,12 +42,7 @@ static char		*ft_strdup(const char *s1)
 
 	if (!(s2 = (char *)malloc((ft_strlen(s1) + 1) * sizeof(char))))
 		return (0);
-	i = 0;
-	while (s1[i])
-	{
-		s2[i] = s1[i];
-		i++;
-	}
+	i = ft_strcpy(s2, s1);
 	s2[i] = '\0';
 	return (s2);
 }
@@ -62,19 +50,18 @@ static char		*ft_strdup(const char *s1)
 char			*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
-	size_t	s1_len;
-	size_t	s2_len;
+	size_t	i;
 
 	if (!s1 && !s2)
 		return (0);
 	if (!s1 || !s2)
-		return (!s1 ? ft_strdup(s2) : 0);
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	if (!(str = (char *)malloc((s1_len + s2_len + 1) * sizeof(char))))
+		return (!s1 ? ft_strdup(s2) : ft_strdup(s1));
+	if (!(str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1)
+														* sizeof(char))))
 		return (0);
-	ft_strlcat(str, s1, s1_len + 1);
-	ft_strlcat(str, s2, s1_len + s2_len + 1);
+	i = ft_strcpy(str, s1);
+	i += ft_strcpy(str + i, s2);
+	str[i] = '\0';
 	return (str);
 }
 
